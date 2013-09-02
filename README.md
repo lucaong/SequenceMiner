302340087fafe56adc34f2674b547402e44860b6
Luca Ongaro <lukeongaro@gmail.com>
2013-09-02 23:59:57 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,53 @@
+SequenceMiner
+=============
+
+A simple and experimental class to mine frequent sequences of contiguous items and generate rules based on them. It performs some operations
+concurrently using Akka actors in a MapReduce fashion.
+
+Example
+-------
+
+```scala
+// Create a SequenceMiner instance using moby_dick.txt as the
+// dataset and separating tokens by non-word characters (to get
+// all the words)
+val miner = new SequenceMiner("moby_dick.txt", """\W+""")
+
+// Get sequences of contiguous words appearing at least 100 times
+val freqSeq = miner.getFrequentSequences(100)
+
+// Get rules from those frequent sequences with minimum
+// confidence of 0.5 and minimum lift of 10
+val rules = miner.getRules(freqSeq, 0.5, 10)
+
+// Print rules:
+rules.foreach( println _ )
+
+/*
+feet, in -> length (s: 10 c: 0.526 l: 1718.076)
+guernsey -> man (s: 11 c: 1.000 l: 476.756)
+a, sperm -> whale (s: 13 c: 0.867 l: 179.698)
+number -> of (s: 15 c: 0.556 l: 20.908)
+dough -> boy (s: 16 c: 0.941 l: 3638.016)
+the, right -> whale (s: 25 c: 0.658 l: 136.411)
+vicinity -> of (s: 11 c: 0.579 l: 21.789)
+supposed -> to (s: 10 c: 0.667 l: 35.714)
+owing -> to (s: 22 c: 0.917 l: 49.107)
+what, do -> you (s: 10 c: 0.625 l: 167.689)
+...
+*/
+```
+
+Anatomy of a Rule
+-----------------
+
+A rule is composed by an antecedent and a consequent. Both of them are sequences. They are also characterized by three values: a support, a
+confidence and a lift. The support is the number of occurrences of the rule; the confidence is the proportion of times that, given the
+antecent, the consequent is found; the lift is how much more frequent the rule is compared to what we would expect if the antecend and the
+consequent were independent events. For example, the following rule:
+
+A, B -> C, D, E (s: 153 c: 0.784 l: 44.513)
+
+Is read as "when A is followed by B, then also C, D and E are likely to follow". Moreover, this rule occurs 153 times. In the 78.4% of the
+case in which A is followed by B, then they are also followed by C, D and E. This association is 44.513 times more frequent than what
+expected if the sequences A, B and C, D, E were independent.
---
+++ src/main/scala/SequenceMiner.scala
@@ -0,0 +1,221 @@
+import scala.io.Source
+import scala.language.implicitConversions
+import scala.language.postfixOps
+import akka.actor.ActorSystem
+
+class SequenceMiner( filePath: String, tokenize: String => Seq[String] ) {
+
+  def this( filePath: String, sep: String = """\s*,\s*""") {
+    this( filePath, ( str ) => str.split( sep ) )
+  }
+
+  case class Rule(
+    antecedent: Seq[String],
+    consequent: Seq[String],
+    support:    Int,
+    confidence: Double,
+    lift:       Double
+  ) {
+    override def toString = {
+      antecedent.mkString(", ") +
+      " -> " + consequent.mkString(", ") +
+      " (s: " + support +
+      " c: %.3f".format( confidence ) +
+      " l: %.3f".format( lift ) + ")"
+    }
+  }
+
+  class ParallelAggregator[A]( iter: Iterator[A], system: ActorSystem ) {
+    import akka.actor.{ Actor, ActorRef, Props }
+
+    case class  Item[B]( z: B, item: Option[A] )
+    case class  Extraction[B]( partialResult: B )
+    case object Combine
+    case class  Done[B]( result: B )
+
+    private class Worker[B]( extract: ( B, A ) => B ) extends Actor {
+      def receive = {
+        case Item(z, None) => 
+          sender ! Done(z)
+        case Item(z, Some(item)) =>
+          sender ! Extraction( extract( z.asInstanceOf[B], item.asInstanceOf[A] ) )
+      }
+    }
+
+    private class Combinator[B](
+      z:       B,
+      extract: ( B, A ) => B,
+      combine: ( B, B ) => B
+    ) extends Actor {
+
+      val workers = Vector.tabulate( 100 ) {
+        ( i ) => context.actorOf( Props( new Worker[B]( extract ) ),
+          "parallel-aggregator-worker-" + i )
+      }
+
+      private var combination   = z
+      private var activeWorkers = workers.size
+      private var caller: ActorRef = _
+
+      def receive = {
+        case Combine => {
+          caller = sender
+          workers.foreach { _ ! Item(z, optionalNext()) }
+          context.become( receiveExtractions )
+        }
+      }
+
+      def receiveExtractions: Receive = {
+        case Extraction(partialResult) =>
+          sender ! Item(partialResult, optionalNext())
+        case Done(result) => {
+          context.stop( sender )
+          activeWorkers -= 1
+          combination = combine( combination, result.asInstanceOf[B] )
+          if ( activeWorkers == 0 ) {
+            this.caller ! combination
+          }
+        }
+      }
+    }
+
+    private def optionalNext(): Option[A] = {
+      if ( iter.hasNext ) Some(iter.next) else None
+    }
+
+    def parallelAggregate[B](z: B)(
+      extract: (B, A) => B,
+      combine: (B, B) => B
+    ): B = {
+      import scala.concurrent.duration._
+      import akka.util.Timeout
+      import akka.pattern.ask
+      import scala.concurrent.Await
+
+      implicit val timeout = Timeout( 10 minutes )
+
+      val combinator = system.actorOf(
+        Props( new Combinator( z, extract, combine ) )
+      )
+
+      val future = combinator ? Combine
+      val result = Await.result( future, Duration.Inf )
+      system.stop( combinator )
+      result.asInstanceOf[B]
+    }
+  }
+
+  val actorSystem = ActorSystem("parallel-aggregator")
+
+  implicit def iteratorToParallelAggregator[A]( iter: Iterator[A] ) = {
+    new ParallelAggregator( iter, actorSystem )
+  }
+
+  private def lineIterator = {
+    Source.fromFile( filePath ).getLines
+  }
+
+  private object countToProbability {
+    import scala.collection.mutable
+    private val cache = mutable.Map[Int, Int]()
+
+    def apply( count: Double, size: Int ) = {
+      count / totalPossibilities( size )
+    }
+
+    private def totalPossibilities( size: Int ) = {
+      if ( !cache.contains( size ) ) {
+        val tot = lineIterator.parallelAggregate(0)(
+          ( sum, line ) => { sum + tokenize( line ).length - size + 1 },
+          ( sum1, sum2 ) => sum1 + sum2
+        )
+        cache( size ) = tot
+      }
+      cache( size )
+    }
+  }
+
+  private def countSequences(
+    n:        Int,
+    filterFn: Seq[String] => Boolean = _ => true
+  ) = {
+    lineIterator.parallelAggregate( Map[Seq[String], Int]() withDefaultValue 0 )(
+      ( counts, line ) => {
+        tokenize( line ).sliding( n ).foldLeft( counts ) {
+          ( counts, item ) => {
+            val sequence = item.toList
+            if ( item.length == n && filterFn( sequence ) )
+              counts.updated( sequence, counts( sequence ) + 1 )
+            else counts
+          }
+        }
+      },
+      ( c1, c2 ) => c1 ++ c2.map { case (k, v) => k -> ( c1( k ) + v ) }
+    )
+  }
+
+  def getFrequentSequences( minSupport: Double ) = {
+    val areSubsFrequent = (
+      n:      Int,
+      seq:    Seq[String],
+      counts: Map[Seq[String], Int]
+    ) => {
+      n == 0 || seq.toIterator.sliding( n ).forall( counts.contains(_) )
+    }
+
+    def addLargerSequences(
+      n:      Int = 1,
+      counts: Map[Seq[String], Int] = Map[Seq[String], Int]()
+    ): Map[Seq[String], Int] = {
+      val more = countSequences( n, areSubsFrequent( n - 1, _, counts ) ).
+        filter( _._2 >= minSupport )
+      if ( !more.isEmpty ) {
+        addLargerSequences( n + 1, counts ++ more )
+      } else {
+        counts
+      }
+    }
+ 
+    addLargerSequences()
+  }
+
+  def getRules(
+    freqSequences: Map[Seq[String], Int],
+    minConfidence: Double,
+    minLift:       Double = 1.0
+  ) = {
+    import scala.collection.mutable
+    val rules = mutable.Set.empty[Rule]
+
+    def addRulesInSequence(
+      sequence: Seq[String],
+      count:    Int,
+      length:   Int,
+      n:        Int = 1
+    ) {
+      val emptySet = mutable.Set.empty[Rule]
+      if ( length > n ) {
+        val ( prec, cons ) = sequence.splitAt( length - n )
+        val confidence     = count.toDouble / freqSequences( prec )
+        val lift = countToProbability( count, length ) / (
+          countToProbability( freqSequences( prec ), length - n ) *
+          countToProbability( freqSequences( cons ), n )
+        )
+        if ( confidence >= minConfidence ) {
+          if ( lift >= minLift )
+            rules += Rule( prec, cons, count, confidence, lift )
+          addRulesInSequence( sequence, count, length, n + 1 )
+        }
+      }
+    }
+
+    for (
+      ( sequence, count ) <- freqSequences
+    ) {
+      val length = sequence.length
+      addRulesInSequence( sequence, count, length )
+    }
+
+    rules.toSet
+  }
+}
